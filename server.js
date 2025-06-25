546bd6de70cb49286155c02245efc1d1186dfbf6
krendun <kafkabre@protonmail.com>
2025-06-24 22:52:37 -0400

    commit, correct async code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt = require('bcrypt');
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -11,7 +12,12 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res);
+    });
+});
 
 //END_ASYNC
 
